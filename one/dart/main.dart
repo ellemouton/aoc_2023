129b66e64997bfb646ca8cadadf58e059b64ba0d
Elle Mouton <elle.mouton@gmail.com>
2023-12-03 19:35:33 +0200

    one/dart: make nicer

--- one/dart/main.dart
+++ one/dart/main.dart
@@ -1,23 +1,38 @@
 import 'dart:io';
 
+const input = "/Users/elle/projects/AOC_2023/one/input.txt";
+
+int total = 0;
+
 void main() {
-  File file = File("/Users/elle/projects/AOC_2023/one/input.txt");
-  List<String> lines = file.readAsLinesSync();
+  partOne();
+}
 
-  int total = 0;
-  for (String line in lines) {
-    String str = line.replaceAll(new RegExp(r'[^0-9]'), '');
-
-    if (str.length > 2) {
-      str = str[0] + str[str.length - 1];
-    } else if (str.length == 1) {
-      str = str[0] + str[0];
-    }
-
-    int n = int.parse(str);
-
-    total += n;
-  }
+void partOne() {
+  forEachLine((line) => lineOp(line));
 
   print(total);
+}
+
+void lineOp(String line) {
+  String str = line.replaceAll(new RegExp(r'[^0-9]'), '');
+
+  if (str.length > 2) {
+    str = str[0] + str[str.length - 1];
+  } else if (str.length == 1) {
+    str = str[0] + str[0];
+  }
+
+  int n = int.parse(str);
+
+  total += n;
+}
+
+void forEachLine(Function(String) f) {
+  File file = File(input);
+  List<String> lines = file.readAsLinesSync();
+
+  for (String line in lines) {
+    f(line);
+  }
 }

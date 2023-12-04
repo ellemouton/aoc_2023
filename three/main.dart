ca125bc0e62f9ef072256bdc8a30916588591050
Elle Mouton <elle.mouton@gmail.com>
2023-12-04 08:53:47 +0200

    three: part 1

--- three/main.dart
+++ three/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
-const input = "/Users/elle/projects/AOC_2023/three/example.txt";
+// const input = "/Users/elle/projects/AOC_2023/three/example.txt";
+const input = "/Users/elle/projects/AOC_2023/three/input.txt";
 
 void main() {
   partOne();
@@ -17,6 +18,7 @@ List<List<String>> inputMap = [];
 
 List<Coordinate> symbols = [];
 
+int totalEngine = 0;
 void partOne() {
   // Construct the 2D map.
   forEachLine(input, (line) => lineOp1(line));
@@ -24,24 +26,49 @@ void partOne() {
   for (int i = 0; i < inputMap.length; i++) {
     int lineNum = 0;
     bool busy = false;
+    bool isEnginePart = false;
 
     for (int j = 0; j < inputMap[i].length; j++) {
       String current = inputMap[i][j];
 
       // Skip symbols and full stops.
       if (current == "." || !isInt(current)) {
-        if (busy) {
-          print(lineNum);
+        if (isEnginePart) {
+          totalEngine += lineNum;
         }
 
         busy = false;
         lineNum = 0;
+        isEnginePart = false;
 
         continue;
       }
 
       // Now we know it is an int.
       int n = int.parse(current);
+
+      // Check if this part of the number makes the greater number an engine
+      // part.
+      for (int y = i - 1; y <= i + 1; y++) {
+        for (int x = j - 1; x <= j + 1; x++) {
+          if (x == j && y == i) {
+            continue;
+          }
+          if (x < 0 ||
+              y < 0 ||
+              y >= inputMap.length ||
+              x >= inputMap[y].length) {
+            continue;
+          }
+
+          String eval = inputMap[y][x];
+          if (eval == "." || isInt(eval)) {
+            continue;
+          }
+
+          isEnginePart = true;
+        }
+      }
 
       // If we were constructing a number before, we continue building the
       // number.
@@ -53,10 +80,12 @@ void partOne() {
       }
     }
 
-    if (busy) {
-      print(lineNum);
+    if (isEnginePart) {
+      totalEngine += lineNum;
     }
   }
+
+  print(totalEngine);
 }
 
 void lineOp1(String line) {

8088f5c5fafdfad7b57dabd97f531545883a8b09
Elle Mouton <elle.mouton@gmail.com>
2023-12-07 07:52:52 +0200

    five: part 2 - brute force - slow

--- five/main.dart
+++ five/main.dart
@@ -3,7 +3,41 @@ import 'dart:io';
 const input = "/Users/elle/projects/AOC_2023/five/input.txt";
 
 void main() {
-  partOne();
+  // partOne();
+  partTwo();
+}
+
+void partTwo() {
+  forEachLine(input, (line) => lineOp1(line));
+  if (map.length > 0) {
+    map.sort((a, b) {
+      if (a.srcStart < b.srcStart) {
+        return -1;
+      }
+      if (a.srcStart > b.srcStart) {
+        return 1;
+      }
+
+      return 0;
+    });
+    mapSet.add(map);
+    map = [];
+  }
+
+  for (int i = 0; i < seeds.length; i += 2) {
+    print("doing seed $i");
+    int current = seeds[i];
+    int range = seeds[i + 1];
+
+    for (int j = 0; j < range; j++) {
+      int loc = getSeedLocation(current + j);
+      if (minLoc == -1 || loc < minLoc) {
+        minLoc = loc;
+      }
+    }
+  }
+
+  print(minLoc);
 }
 
 class FromTo {
@@ -14,7 +48,7 @@ class FromTo {
   FromTo(this.srcStart, this.destStart, this.range);
 
   display() {
-    print("$srcStart, $destStart, $range");
+    print("$destStart, $srcStart, $range");
   }
 }
 
@@ -67,7 +101,7 @@ int getSeedLocation(int seed) {
 int getDest(int input, List<FromTo> map) {
   for (FromTo range in map) {
     int end = range.srcStart + range.range;
-    if (input >= range.srcStart && input <= end) {
+    if (input >= range.srcStart && input < end) {
       int diff = input - range.srcStart;
 
       return range.destStart + diff;

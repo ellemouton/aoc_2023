d8a186992f1d2e73bc175c3c6e073a40e81fc1ef
Elle Mouton <elle.mouton@gmail.com>
2023-12-10 07:25:45 +0200

    nine: part 2

--- nine/main.dart
+++ nine/main.dart
@@ -3,23 +3,68 @@ import 'dart:io';
 const input = "/Users/elle/projects/AOC_2023/nine/input.txt";
 
 void main() {
-  partOne();
+  // partOne();
+  partTwo();
 }
 
 List<List<int>> inputs = [];
+
+void partTwo() {
+  forEachLine(input, (line) => lineOp1(line));
+
+  int count = 0;
+  for (List<int> set in inputs) {
+    count += analyseBack(set);
+  }
+
+  print(count);
+}
+
+int analyseBack(List<int> set) {
+  List<int> current = set;
+  bool done = false;
+  List<int> firstItemInEachSet = [];
+
+  while (!done) {
+    List<int> nextSet = [];
+    firstItemInEachSet.add(current[0]);
+
+    for (int i = 0; i < current.length - 1; i++) {
+      nextSet.add(current[i + 1] - current[i]);
+    }
+
+    current = nextSet;
+
+    int sum = 0;
+    nextSet.forEach((element) {
+      sum += element;
+    });
+
+    if (sum == 0) {
+      done = true;
+    }
+  }
+
+  int next = 0;
+  for (int i = firstItemInEachSet.length - 1; i >= 0; i--) {
+    next = firstItemInEachSet[i] - next;
+  }
+
+  return next;
+}
 
 void partOne() {
   forEachLine(input, (line) => lineOp1(line));
 
   int count = 0;
   for (List<int> set in inputs) {
-    count += analyse(set);
+    count += analyseFwd(set);
   }
 
   print(count);
 }
 
-int analyse(List<int> set) {
+int analyseFwd(List<int> set) {
   List<int> current = set;
   bool done = false;
   List<int> lastItemInEachSet = [];

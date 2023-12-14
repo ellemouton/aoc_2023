3406cf3055ee1d5f1105f87229ba0aaf40a09e05
Elle Mouton <elle.mouton@gmail.com>
2023-12-14 08:45:02 +0200

    fourteen: part 2: brute force too slow

--- fourteen/main.dart
+++ fourteen/main.dart
@@ -1,29 +1,128 @@
 import 'dart:io';
 
-const input = "/Users/elle/projects/AOC_2023/fourteen/input.txt";
+const input = "/Users/elle/projects/AOC_2023/fourteen/example.txt";
 
 void main() {
-  partOne();
+  // partOne();
+  partTwo();
 }
 
-List<List<String>> flipped = [];
+void partTwo() {
+  forEachLine(input, (line) => lineOp1(line));
+
+  // Rotate anti clockwise once to get North to be left.
+  rotateMapAntiClockWise();
+
+  int cycles = 1000000000;
+
+  for (int i = 0; i < cycles; i++) {
+    print("cycle: $i");
+    map.forEach((line) {
+      tilt(line);
+    });
+
+    // Rotate clockwise to get West left.
+    rotateMapClockWise();
+
+    map.forEach((line) {
+      tilt(line);
+    });
+
+    // Rotate clockwise to get South left.
+    rotateMapClockWise();
+
+    map.forEach((line) {
+      tilt(line);
+    });
+
+    // Rotate clockwise to get East left.
+    rotateMapClockWise();
+
+    map.forEach((line) {
+      tilt(line);
+    });
+
+    // Rotate clockwise to get North left.
+    rotateMapClockWise();
+  }
+
+  // rotate once more to get North at the top.
+  rotateMapClockWise();
+
+  print(calcLoad());
+}
+
+void rotateMapClockWise() {
+  List<List<String>> newMap = [];
+  int xLen = map[0].length;
+
+  for (int i = 0; i < xLen; i++) {
+    newMap.add([]);
+
+    for (int j = map.length - 1; j >= 0; j--) {
+      String itemToMove = map[j][i];
+
+      newMap[i].add(itemToMove);
+    }
+  }
+  map = newMap;
+}
+
+void rotateMapAntiClockWise() {
+  List<List<String>> newMap = [];
+  for (int i = 0; i < map.length; i++) {
+    for (int j = map[i].length - 1; j >= 0; j--) {
+      String itemToMove = map[i][j];
+
+      // new x = old y.
+      int newX = j;
+
+      // new y = xlen-oldx
+      int newY = map[i].length - j;
+
+      while (newMap.length <= newY) {
+        newMap.add([]);
+      }
+
+      newMap[newY].add(itemToMove);
+    }
+  }
+  map = newMap.sublist(1);
+}
 
 void partOne() {
   forEachLine(input, (line) => lineOp1(line));
 
-  // I flipped the input, so rolling north is now just rolling left
-  // which can just be solved line by line.
-  int totalLoad = 0;
-  flipped.forEach((line) {
-    totalLoad += calcLoad(line);
+  // Rotate anti clockwise.
+  rotateMapAntiClockWise();
+
+  map.forEach((line) {
+    tilt(line);
   });
 
-  print(totalLoad);
+  // Rotate it back.
+  rotateMapClockWise();
+
+  print(calcLoad());
 }
 
-int calcLoad(List<String> line) {
-  int barrier = -1;
+int calcLoad() {
   int load = 0;
+  for (int i = 0; i < map.length; i++) {
+    for (int j = 0; j < map[i].length; j++) {
+      if (map[i][j] != "O") {
+        continue;
+      }
+
+      load += map.length - i;
+    }
+  }
+
+  return load;
+}
+
+void tilt(List<String> line) {
+  int barrier = -1;
   for (int i = 0; i < line.length; i++) {
     if (line[i] == "#") {
       barrier = i;
@@ -54,21 +153,14 @@ int calcLoad(List<String> line) {
       }
 
       barrier = finalPos;
-      load += line.length - (finalPos);
     }
   }
-
-  return load;
 }
 
-void lineOp1(String line) {
-  for (int i = 0; i < line.length; i++) {
-    if (i == flipped.length) {
-      flipped.add([]);
-    }
+List<List<String>> map = [];
 
-    flipped[i].add(line[i]);
-  }
+void lineOp1(String line) {
+  map.add(line.split(""));
 }
 
 void forEachLine(String input, Function(String) f) {

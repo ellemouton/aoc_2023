0241633224995aa004a0d478c5da8e9dc248d184
Elle Mouton <elle.mouton@gmail.com>
2023-12-15 08:57:41 +0200

    fifteen: part 2

--- fifteen/main.dart
+++ fifteen/main.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 const input = "/Users/elle/projects/AOC_2023/fifteen/input.txt";
 
 void main() {
-  partOne();
+  // partOne();
+  partTwo();
 }
 
 int hash(String input) {
@@ -16,6 +17,79 @@ int hash(String input) {
   }
 
   return current;
+}
+
+class Box {
+  // map from label to focal length.
+  Map<String, int> lenses = {};
+
+  List<String> orderedLenses = [];
+
+  void add(String label, int len) {
+    if (lenses[label] != null) {
+      lenses[label] = len;
+      return;
+    }
+
+    lenses[label] = len;
+    orderedLenses.add(label);
+  }
+
+  void remove(String label) {
+    if (lenses[label] == null) {
+      return;
+    }
+
+    lenses.remove(label);
+    orderedLenses.remove(label);
+  }
+}
+
+void partTwo() {
+  String line = "";
+  forEachLine(input, (l) => line = l);
+
+  List<String> seq = line.split(",");
+
+  // Box set. box num -> box.
+  Map<int, Box> set = {};
+
+  seq.forEach((line) {
+    if (line.contains("-")) {
+      String label = line.substring(0, line.indexOf("-"));
+      int boxNum = hash(label);
+
+      if (set[boxNum] == null) {
+        set[boxNum] = Box();
+      }
+
+      set[boxNum]!.remove(label);
+
+      return;
+    }
+
+    String label = line.substring(0, line.indexOf("="));
+    int boxNum = hash(label);
+    int focalLen = int.parse(line.substring(line.indexOf("=") + 1));
+
+    if (set[boxNum] == null) {
+      set[boxNum] = Box();
+    }
+
+    set[boxNum]!.add(label, focalLen);
+  });
+
+  int total = 0;
+  set.forEach((boxNum, box) {
+    for (int i = 0; i < box.orderedLenses.length; i++) {
+      int focusPower =
+          (1 + boxNum) * (i + 1) * box.lenses[box.orderedLenses[i]]!;
+
+      total += focusPower;
+    }
+  });
+
+  print(total);
 }
 
 void partOne() {

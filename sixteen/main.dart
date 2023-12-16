a7b8d75a4006a7e9a34a8c9bbbe346d6d787da62
Elle Mouton <elle.mouton@gmail.com>
2023-12-16 10:24:41 +0200

    sixteen: optimize part 1 for part 2

--- sixteen/main.dart
+++ sixteen/main.dart
@@ -3,14 +3,24 @@ import 'dart:io';
 const input = "/Users/elle/projects/AOC_2023/sixteen/input.txt";
 
 void main() {
-  partOne();
+  // partOne();
+  partTwo();
 }
 
 List<List<String>> map = [];
 
 Map<String, bool> energised = {};
 
-Map<String, bool> visited = {};
+Map<String, int> visited = {};
+
+void partTwo() {
+  forEachLine(input, (line) => lineOp1(line));
+
+  int count = traverse(Coord(0, 0, direction.right));
+
+  print(energised.length);
+  print(count);
+}
 
 void partOne() {
   forEachLine(input, (line) => lineOp1(line));
@@ -50,18 +60,47 @@ class Coord {
     }
   }
 
-  mark() {
+  bool marked() {
+    String key = "$x-$y";
+
+    return energised[key] == true;
+  }
+
+  markBusy() {
     String key = "$x-$y";
     energised[key] = true;
 
     String keyWithDirection = "$x-$y-$d";
-    visited[keyWithDirection] = true;
+
+    if (visited[keyWithDirection] != null) {
+      throw "bad";
+    }
+
+    visited[keyWithDirection] = -1;
+  }
+
+  markEnergised(int num) {
+    String keyWithDirection = "$x-$y-$d";
+
+    visited[keyWithDirection] = num;
+  }
+
+  bool partOfThisLoop() {
+    String keyWithDirection = "$x-$y-$d";
+
+    return visited[keyWithDirection] == -1;
+  }
+
+  int getCount() {
+    String keyWithDirection = "$x-$y-$d";
+
+    return visited[keyWithDirection]!;
   }
 
   bool alreadyDone() {
     String keyWithDirection = "$x-$y-$d";
 
-    return visited[keyWithDirection] == true;
+    return visited[keyWithDirection] != null;
   }
 
   Coord moveUp() {
@@ -85,81 +124,93 @@ class Coord {
   }
 }
 
-void traverse(Coord c) {
+int traverse(Coord c) {
   // Stopping conditions:
   // 1) the coordinate is off the map.
   if (c.x < 0 || c.y < 0 || c.x >= map[0].length || c.y >= map.length) {
-    return;
+    return 0;
   }
 
-  // 2) This coordinate hase been visited already with the
-  // same trajectory.
+  // 2) We've entered this tile in this direction in the current loop we are on
+  // (in other words we would cycle if we didnt stop now).
+  if (c.partOfThisLoop()) {
+    return 0;
+  }
+
+  // 3) We've checked this tile in this direction in the past (in another loop)
+  // and so we can just get the already calculated count from there.
   if (c.alreadyDone()) {
-    return;
+    return c.getCount();
   }
 
-  c.mark();
+  int totalEnergised = 1;
+  if (c.marked()) {
+    totalEnergised = 0;
+  }
 
-  // Make a copy of what is at the tile currently.
-  String tile = map[c.y][c.x];
+  c.markBusy();
 
   // Now, we need to check where to traverse to next after this point. That
   // very much depends on what is at this point.
-  switch (tile) {
+  switch (map[c.y][c.x]) {
     // If this is a "." then we just continue the same trajectory.
     case ".":
-      return traverse(c.move(c.d));
+      totalEnergised += traverse(c.move(c.d));
 
     // If approaching from left/right then continue. Else split with new
     // left & right trajectories.
     case "-":
       if (c.d == direction.left || c.d == direction.right) {
-        return traverse(c.move(c.d));
+        totalEnergised += traverse(c.move(c.d));
+      } else {
+        totalEnergised += traverse(c.move(direction.left));
+        totalEnergised += traverse(c.move(direction.right));
       }
-
-      traverse(c.move(direction.left));
-
-      return traverse(c.move(direction.right));
 
     case "|":
       if (c.d == direction.up || c.d == direction.down) {
-        return traverse(c.move(c.d));
+        totalEnergised += traverse(c.move(c.d));
+      } else {
+        totalEnergised += traverse(c.move(direction.up));
+        totalEnergised += traverse(c.move(direction.down));
       }
-
-      traverse(c.move(direction.up));
-
-      return traverse(c.move(direction.down));
 
     case "\\":
       switch (c.d) {
         case direction.up:
-          return traverse(c.move(direction.left));
+          totalEnergised += traverse(c.move(direction.left));
 
         case direction.down:
-          return traverse(c.move(direction.right));
+          totalEnergised += traverse(c.move(direction.right));
 
         case direction.left:
-          return traverse(c.move(direction.up));
+          totalEnergised += traverse(c.move(direction.up));
 
         case direction.right:
-          return traverse(c.move(direction.down));
+          totalEnergised += traverse(c.move(direction.down));
       }
 
     case "/":
       switch (c.d) {
         case direction.up:
-          return traverse(c.move(direction.right));
+          totalEnergised += traverse(c.move(direction.right));
 
         case direction.down:
-          return traverse(c.move(direction.left));
+          totalEnergised += traverse(c.move(direction.left));
 
         case direction.left:
-          return traverse(c.move(direction.down));
+          totalEnergised += traverse(c.move(direction.down));
 
         case direction.right:
-          return traverse(c.move(direction.up));
+          totalEnergised += traverse(c.move(direction.up));
       }
   }
+
+  if (!c.marked()) {
+    c.markEnergised(totalEnergised);
+  }
+
+  return totalEnergised;
 }
 
 void lineOp1(String line) {

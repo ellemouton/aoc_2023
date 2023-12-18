b91c3dfe3d88f9d3581a00dd220592df0074c87b
Elle Mouton <elle.mouton@gmail.com>
2023-12-18 10:11:37 +0200

    eighteen: part 1. Uses helpers from q 10.

---
+++ eighteen/main.dart
@@ -0,0 +1,170 @@
+import 'dart:io';
+import 'ten.dart';
+
+const input = "/Users/elle/projects/AOC_2023/eighteen/input.txt";
+
+void main() {
+  partOne();
+}
+
+enum direction { left, right, up, down }
+
+List<Coord> trench = [];
+
+class Coord {
+  int x;
+  int y;
+  String symbol;
+
+  Coord(this.x, this.y, this.symbol);
+
+  Coord move(direction d, direction nextDir) {
+    switch (d) {
+      case direction.up:
+        if (nextDir == direction.up) {
+          return Coord(x, y - 1, "|");
+        }
+        if (nextDir == direction.left) {
+          return Coord(x, y - 1, "7");
+        }
+        if (nextDir == direction.right) {
+          return Coord(x, y - 1, "F");
+        }
+        throw ("bad");
+
+      case direction.down:
+        if (nextDir == direction.down) {
+          return Coord(x, y + 1, "|");
+        }
+        if (nextDir == direction.left) {
+          return Coord(x, y + 1, "J");
+        }
+        if (nextDir == direction.right) {
+          return Coord(x, y + 1, "L");
+        }
+        throw ("bad");
+
+      case direction.left:
+        if (nextDir == direction.left) {
+          return Coord(x - 1, y, "-");
+        }
+        if (nextDir == direction.down) {
+          return Coord(x - 1, y, "F");
+        }
+        if (nextDir == direction.up) {
+          return Coord(x - 1, y, "L");
+        }
+        throw ("bad");
+
+      case direction.right:
+        if (nextDir == direction.right) {
+          return Coord(x + 1, y, "-");
+        }
+        if (nextDir == direction.down) {
+          return Coord(x + 1, y, "7");
+        }
+        if (nextDir == direction.up) {
+          return Coord(x + 1, y, "J");
+        }
+        throw ("bad");
+    }
+  }
+}
+
+class Instruction {
+  late direction d;
+  final int count;
+
+  Instruction(String dir, this.count) {
+    switch (dir) {
+      case "R":
+        d = direction.right;
+      case "L":
+        d = direction.left;
+      case "U":
+        d = direction.up;
+      case "D":
+        d = direction.down;
+    }
+  }
+
+  display() {
+    print("$d: $count");
+  }
+}
+
+List<Instruction> instructs = [];
+
+void partOne() {
+  forEachLine(input, (line) => lineOp1(line));
+
+  Coord current = Coord(0, 0, "F");
+  //trench.add(current);
+  int minY = 0;
+  int minX = 0;
+  int maxX = 0;
+
+  for (int j = 0; j < instructs.length; j++) {
+    // Get next direction.
+    direction nextDir = instructs[0].d;
+    if (j + 1 < instructs.length) {
+      nextDir = instructs[j + 1].d;
+    }
+
+    for (int i = 0; i < instructs[j].count; i++) {
+      direction nd = instructs[j].d;
+      if (i == instructs[j].count - 1) {
+        nd = nextDir;
+      }
+      current = current.move(instructs[j].d, nd);
+      trench.add(current);
+
+      if (current.x < minX) {
+        minX = current.x;
+      }
+
+      if (current.x > maxX) {
+        maxX = current.x;
+      }
+
+      if (current.y < minY) {
+        minY = current.y;
+      }
+    }
+  }
+
+  trench.forEach((coord) {
+    coord.x += minX.abs();
+    coord.y += minY.abs();
+    maxX += minX.abs();
+  });
+
+  trench.forEach((coord) {
+    while (maze.length <= coord.y) {
+      maze.add([]);
+    }
+
+    while (maze[coord.y].length <= maxX) {
+      maze[coord.y].add(Pipe("."));
+    }
+
+    maze[coord.y][coord.x].symbol = coord.symbol;
+    maze[coord.y][coord.x].isLoopTile = true;
+  });
+
+  print(trench.length + countInside());
+}
+
+void lineOp1(String line) {
+  line = line.substring(0, line.indexOf("(") - 1);
+  List<String> parts = line.split(" ");
+
+  instructs.add(Instruction(parts[0], int.parse(parts[1])));
+}
+
+void forEachLine(String input, Function(String) f) {
+  File file = File(input);
+  List<String> lines = file.readAsLinesSync();
+
+  lines.forEach((line) => f(line));
+}
---
+++ eighteen/ten.dart
@@ -0,0 +1,182 @@
+import 'dart:io';
+
+class Coord {
+  int x;
+  int y;
+
+  Coord(this.x, this.y) {}
+
+  display() {
+    print("($x, $y)");
+  }
+
+  bool equal(Coord other) {
+    return this.x == other.x && this.y == other.y;
+  }
+}
+
+class Quad {
+  // "true" for any of these means: inside.
+  bool topLeft;
+  bool topRight;
+  bool bottomLeft;
+  bool bottomRight;
+
+  Quad(this.topLeft, this.bottomLeft, this.topRight, this.bottomRight);
+}
+
+class Pipe {
+  String symbol;
+  bool isLoopTile = false;
+  Quad? quad;
+
+  Pipe(this.symbol);
+
+  bool isS() {
+    return symbol == "S";
+  }
+
+  setQuad(bool tl, bool tr, bool bl, bool br) {
+    quad = Quad(tl, tr, bl, br);
+  }
+
+  bool canLookLeft() {
+    return !isLoopTile && (isS() || lookingLeft.contains(symbol));
+  }
+
+  bool canLookRight() {
+    return !isLoopTile && (isS() || lookingRight.contains(symbol));
+  }
+
+  bool canLookUp() {
+    return !isLoopTile && (isS() || lookingUp.contains(symbol));
+  }
+
+  bool canLookDown() {
+    return !isLoopTile && (isS() || lookingDown.contains(symbol));
+  }
+}
+
+const List<String> lookingDown = ["|", "7", "F"];
+const List<String> lookingUp = ["|", "L", "J"];
+const List<String> lookingLeft = ["-", "J", "7"];
+const List<String> lookingRight = ["-", "F", "L"];
+
+List<List<Pipe>> maze = [];
+late Coord start;
+
+int countInside() {
+  // Now, we go go from the top to bottom of the map in order to figure out
+  // which tiles are _in_ the loop. Since there is no way that the edge tiles
+  // can be in the loop, those are set to "outside". Then the first time we hit
+  // a loop tile, we switch to inside & start counting any non loop tiles for
+  // that column. When we then hit another loop tile, we switch to outside
+  // again and so on and so on.
+  int insideCount = 0;
+  for (int j = 0; j < maze.length; j++) {
+    Quad prevQuad = Quad(false, false, false, false);
+
+    for (int i = 0; i < maze[j].length; i++) {
+      Pipe p = maze[j][i];
+
+      // If this is not a loop tile, then we should be able to derive the
+      // inside/outside value by looking at the previous tile's top right or
+      // bottom right quads.
+      if (!p.isLoopTile) {
+        if (prevQuad.topRight != prevQuad.bottomRight) {
+          throw "unexpected";
+        }
+
+        bool inside = prevQuad.topRight;
+
+        Quad newQuad = Quad(inside, inside, inside, inside);
+
+        if (inside) {
+          insideCount++;
+        }
+
+        prevQuad = newQuad;
+        continue;
+      }
+
+      // This is a loop pipe. So now we set quads based on the previous tile's
+      // quads and the structure of this pipe.
+
+      if (p.symbol == "|") {
+        if (prevQuad.topRight != prevQuad.bottomRight) {
+          throw "unexpected";
+        }
+
+        bool o = prevQuad.topRight;
+
+        Quad newQuad = Quad(o, o, !o, !o);
+
+        prevQuad = newQuad;
+        continue;
+      }
+
+      if (p.symbol == "-") {
+        if (prevQuad.topRight != !prevQuad.bottomRight) {
+          throw "unexpected";
+        }
+
+        bool o = prevQuad.topRight;
+
+        Quad newQuad = Quad(o, !o, o, !o);
+
+        prevQuad = newQuad;
+        continue;
+      }
+
+      if (p.symbol == "F") {
+        if (prevQuad.topRight != prevQuad.bottomRight) {
+          throw "unexpected";
+        }
+
+        bool o = prevQuad.topRight;
+
+        Quad newQuad = Quad(o, o, o, !o);
+        prevQuad = newQuad;
+        continue;
+      }
+
+      if (p.symbol == "J") {
+        if (prevQuad.topRight != !prevQuad.bottomRight) {
+          throw "unexpected";
+        }
+
+        bool o = prevQuad.topRight;
+
+        Quad newQuad = Quad(o, !o, !o, !o);
+        prevQuad = newQuad;
+        continue;
+      }
+
+      if (p.symbol == "7") {
+        if (prevQuad.topRight != !prevQuad.bottomRight) {
+          throw "unexpected";
+        }
+
+        bool o = prevQuad.topRight;
+
+        Quad newQuad = Quad(o, !o, o, o);
+        prevQuad = newQuad;
+        continue;
+      }
+
+      if (p.symbol == "L") {
+        if (prevQuad.topRight != prevQuad.bottomRight) {
+          throw "unexpected";
+        }
+
+        bool o = prevQuad.topRight;
+
+        Quad newQuad = Quad(o, o, !o, o);
+        prevQuad = newQuad;
+        continue;
+      }
+    }
+  }
+
+  return insideCount;
+}
